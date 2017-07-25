c1c822839f55a965637837248a98b995097ca699
rotemdan <rotemdan@gmail.com>
2017-07-25 08:18:46 +0300

    Updated readme and script description. Reformatted code.

--- README.md
+++ README.md
@@ -1,34 +1,31 @@
 # YouTube Cinema Mode
 
-Improves the YouTube experience by maximizing video player real-estate and fixing some common annoyances.
+Maximizes YouTube's video player to fill the entire browser viewport and fixes a few minor annoyances:
 
-* Maximizes player to fit the entire viewport area.
-* Auto-hides the top bar on watch pages for maximum space utilization (will be hidden when the page is scrolled to the top, and can also be toggled with the escape key - which will also focus the search bar).
-* Auto-pauses other playing videos (in any tab) when playback is started/resumed (supports both watch pages and channel pages).
-* Disables SPF (Structured Page Fragments, AKA red loading bar).
-* Auto-focuses the video player to allow keyboard controls to be immediately effective. This happens on load and whenever the player fills the viewport.
+* Disables SPF (Structured Page Fragments), which also disables the thin red loading bar animation. This is required, in general, for the script to load reliably.
+* Auto-hides the top bar on watch pages for even greater player size - will be automatically hidden when the page is scrolled to the top, though can be toggled with the escape key (which will also focus the search bar).
+* Auto-pauses any existing playing YouTube video (in any tab) when playback is started/resumed (supports both watch pages and channel pages).
+* Auto-focuses the video player to enable player keyboard controls whenever the watch page is scrolled to the top (also adds the additional shortcuts ctrl+left/ctrl+right to navigate to previous/next video).
 * Auto-expands video description.
-
-Supports the HTML5 player only. Tested with both the latest Firefox and Chrome (using the latest Greasemonkey/Tampermonkey). Works with both the current (red/black) player and the newer, transparent one, which can be enabled [here](https://www.youtube.com/testtube) (recommended, and seems to be less buggy in chrome).
 
 ## Installation
 
-* Install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (Firefox) or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (Chrome).
-* Click [this link](https://github.com/rotemdan/YouTubeCinemaMode/raw/master/YouTube_Cinema_Mode.user.js) and then the "install" button when it appears.
-
-## Running alongside other YouTube scripts
-
-Because of duplicate and conflicting behaviours, it doesn't seem to work well alongside [YouTube Center](https://github.com/YePpHa/YouTubeCenter) (depending on the particular YTC settings used). Other YouTube scripts may or may not work correctly in parallel.
-
-## Reusing the code
-
-I tried to organize the code so other developers could benefit from individual features for use in other scripts. Feel free to fork or simply copy/paste into your own work.
+1. Install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) (Firefox) or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) (Chrome).
+2. Click [this link](https://github.com/rotemdan/YouTubeCinemaMode/raw/master/YouTube_Cinema_Mode.user.js) and then the "install" button when it appears.
 
 ## New features and customization
 
-The script is designed as a basic one-size-fits-all solution to improve the YouTube experience for some people. There are currently no plans to add major new features or customization (say, similarly to highly customizable scripts such as [YouTube Center](https://github.com/YePpHa/YouTubeCenter)) as it would be difficult and time-consuming to maintain in the long term. Improvements/expansions of the implementation of the current features and behaviours are planned and suggestions are welcome.
+The script is designed as a basic one-size-fits-all set of modifications to enhance the YouTube experience for some people (but possibly wouldn't fit everyone), originally made for my own personal use. There are no current plans to add major new features or any customization options.
+
+## Running alongside other YouTube enhancements scripts
+
+It may or may not work side-by-side with other YouTube scripts or extensions, conflicting behaviors may possibly be avoided by configuring the secondary script(s).
+
+## Reusing the code
+
+I tried to organize the code so other developers could learn and benefit from individual features for use in other scripts. Feel free to fork or simply copy/paste into your own work.
 
 ## Version history
 
-* 0.2: Added video autofocus. Fixed playlist positioning (in most cases). Changed top bar autohide behavior on watch pages (see description). 
-* 0.1: Initial release.
+* _0.2.x_: Added video autofocus. Fixed playlist positioning (for most cases). Changed top bar autohide behavior on watch pages (see description).
+* _0.1.x_: Initial release.
--- YouTube_Cinema_Mode.user.js
+++ YouTube_Cinema_Mode.user.js
@@ -1,14 +1,14 @@
 ﻿// ==UserScript==
 // @name        YouTube Cinema Mode
-// @description Improves the YouTube experience by maximizing video player real-estate and fixing some common annoyances.
+// @description Maximizes YouTube's video player to fill the entire browser viewport and fixes a few minor annoyances
 // @license     MIT
 // @author      Rotem Dan <rotemdan@gmail.com>
 // @match       https://www.youtube.com/*
-// @version     0.2.2
+// @version     0.2.4
 // @run-at      document-start
 // @grant       none
 // @require     https://code.jquery.com/jquery-2.1.4.min.js
-// @namespace   https://greasyfork.org/en/users/4614
+// @namespace   https://github.com/rotemdan
 // @homepageURL https://github.com/rotemdan/YouTubeCinemaMode
 // ==/UserScript==
 
@@ -25,51 +25,43 @@ $(document).on("ready", onDocumentEnd);
 $(window).on("load", onWindowLoad);
 
 // Operations that are run as early as possible during page load
-function onDocumentStart()
-{
+function onDocumentStart() {
 	installUnsafewindowPolyfill();
 	disableSPF();
 
-	if (isWatchPage)
-	{
+	if (isWatchPage) {
 		disableMatchMedia();
 		installFullSizePlayerStylesheet();
 	}
 }
 
 // Operations that run when DOMContentLoaded event is fired
-function onDocumentEnd()
-{
-	if (isWatchPage)
-	{
+function onDocumentEnd() {
+	if (isWatchPage) {
 		installTopBarAutohide();
 		expandVideoDescription();
 		installPlayerAutoFocus();
 		installPlayerKeyboardShortcutExtensions();
 		installPlaylistRepositioner();
 	}
-	
-	installPlayerAutoPause();	
+
+	installPlayerAutoPause();
 }
 
 // Operations that run when window is loaded (after YouTube scripts are run)
-function onWindowLoad()
-{
-	if (isWatchPage)
-	{
+function onWindowLoad() {
+	if (isWatchPage) {
 		setTimeout(switchPlayerToTheaterMode, 2000);
-    }
+	}
 }
 
 ////////////////////////////////////////////////////////////////
 // Methods
 ////////////////////////////////////////////////////////////////
 
-// Ensure unsafeWindow object is available both in firefox and chrome
-function installUnsafewindowPolyfill()
-{
-	if (typeof unsafeWindow === 'undefined')
-	{
+// Ensure unsafeWindow object is available both in Firefox and Chrome
+function installUnsafewindowPolyfill() {
+	if (typeof unsafeWindow === 'undefined') {
 		if (typeof XPCNativeWrapper === 'function' && typeof XPCNativeWrapper.unwrap === 'function')
 			unsafeWindow = XPCNativeWrapper.unwrap(window);
 		else if (window.wrappedJSObject)
@@ -77,123 +69,112 @@ function installUnsafewindowPolyfill()
 	}
 }
 
-// Disable SPF (Structured Page Fragments), which prevents properly attaching to page load events when navigation occurs 
+// Disable SPF (Structured Page Fragments), which prevents properly attaching to page load events when navigation occurs
 // Will also disable the red loading bar.
-function disableSPF()
-{
-	if (unsafeWindow._spf_state && unsafeWindow._spf_state.config)
-	{
+function disableSPF() {
+	if (unsafeWindow._spf_state && unsafeWindow._spf_state.config) {
 		unsafeWindow._spf_state.config['navigate-limit'] = 0;
 		unsafeWindow._spf_state.config['navigate-part-received-callback'] = function (targetUrl) { location.href = targetUrl; }
 	}
-	
+
 	setTimeout(disableSPF, 50);
 }
 
 // Disable matchMedia - allow proper resizing of the video player and its contents.
-function disableMatchMedia()
-{
+function disableMatchMedia() {
 	window.matchMedia = undefined;
 }
 
-// Add full-size player page stylesheet (works correctly only in theater mode).
-function installFullSizePlayerStylesheet()
-{
-	$("head").append(
-		"<style type='text/css'>" +
-		"body { overflow-x: hidden !important; overflow-y: scroll !important; }" +
-		"#masthead-positioner-height-offset { display: none }" +
-		"#masthead-positioner { visibility: hidden; opacity: 0; transition: opacity 0.2s ease-in-out; }" +
-        ".player-width { width: 100% !important; margin: 0px !important; left: 0px !important; right: 0px !important; }" +		
-		".player-height { height: 100vh !important; }" +
-		":focus { outline: 0; }" +
-		"</style>");
+// Add full-size player page stylesheet (works correctly only when the player is in "theater" mode).
+function installFullSizePlayerStylesheet() {
+	var runningInChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+	var styleSheet =
+		"<style type='text/css'>\n" +
+		"body { overflow-x: hidden !important; }\n" +
+		"#masthead-positioner-height-offset { display: none }\n" +
+		"#masthead-positioner { visibility: hidden; opacity: 0; transition: opacity 0.2s ease-in-out; }\n" +
+		".player-width { width: 100% !important; margin: 0px !important; left: 0px !important; right: 0px !important; }\n" +
+		".player-height { height: 100vh !important; }\n" +
+		":focus { outline: 0; }\n";
+
+	//if (!runningInChrome)
+	styleSheet += "body { overflow-y: scroll !important; }\n";
+
+	styleSheet += "</style>\n";
+
+	$("head").append(styleSheet);
 }
 
 // Switch player to theater mode if in default mode.
-function switchPlayerToTheaterMode()
-{
+function switchPlayerToTheaterMode() {
 	if ($("div#page").hasClass("watch-non-stage-mode"))
 		$("button.ytp-size-button, div.ytp-size-toggle-large").click();
 }
 
 // Automatically shows/hides the top bar based on different properties of the view.
-function installTopBarAutohide()
-{
+function installTopBarAutohide() {
 	var topBar = getTopBar();
 	var videoPlayer = getVideoPlayer();
 	var videoPlayerElement = videoPlayer[0];
-	
-	function topBarIsVisible()
-	{
+
+	function topBarIsVisible() {
 		return topBar.css("visibility") === "visible";
 	}
-	
-	function showTopBar()
-	{
+
+	function showTopBar() {
 		topBar.css("visibility", "visible");
 		topBar.css("opacity", "1");
 	}
-	
-	function hideTopBar()
-	{
+
+	function hideTopBar() {
 		topBar.css("opacity", "0");
 		topBar.css("visibility", "hidden");
 	}
-	
-	function toggleTopBar()
-	{
+
+	function toggleTopBar() {
 		if (topBarIsVisible())
 			hideTopBar();
 		else
 			showTopBar();
 	}
-	
-	function getScrollTop()
-	{
+
+	function getScrollTop() {
 		return $(document).scrollTop();
 	}
-	
-	function onPageScroll()
-	{
+
+	function onPageScroll() {
 		if (getScrollTop() > 0)
 			showTopBar();
 		else
 			hideTopBar();
 	}
-	
+
 	var searchInput = $("input#masthead-search-term");
-	function onKeyDown(e)
-	{
-		if (e.which === 27)
-		{
-			if (getScrollTop() === 0)
-			{
+	function onKeyDown(e) {
+		if (e.which === 27) { // Handle escape key
+			if (getScrollTop() === 0) {
 				toggleTopBar();
-				
+
 				if (topBarIsVisible())
 					setTimeout(function () { searchInput.focus() }, 200);
-				
+
 				e.stopPropagation();
 			}
-		}	
+		}
 	}
-	
+
 	searchInput.on("keydown", onKeyDown);
 	$(document).on("keydown", onKeyDown);
 	$(document).on("scroll", onPageScroll);
 }
 
 // Continuously auto-focus the player when some conditions are met.
-function installPlayerAutoFocus()
-{
-	function autoFocusIfNeeded()
-	{
+function installPlayerAutoFocus() {
+	function autoFocusIfNeeded() {
 		var topbarVisibility = getTopBar().css("visibility");
-		if (topbarVisibility !== undefined)
-		{
-			if (topbarVisibility === "hidden")
-			{
+		if (topbarVisibility !== undefined) {
+			if (topbarVisibility === "hidden") {
 				$(".html5-video-player").focus();
 			}
 			//else if ($(".html5-video-player").is(":focus") && $(document).scrollTop() > 0)
@@ -201,30 +182,24 @@ function installPlayerAutoFocus()
 			//	$(".html5-video-player").blur();
 			//}
 		}
-		
+
 		setTimeout(autoFocusIfNeeded, 20);
 	}
-	
+
 	autoFocusIfNeeded();
 }
 
-function installPlayerKeyboardShortcutExtensions()
-{
+function installPlayerKeyboardShortcutExtensions() {
 	// Install keyboard shortcut extensions
-	function onPlayerKeyDown(e)
-	{
-		if ($(".html5-video-player").is(":focus"))
-		{
-			if (e.ctrlKey)
-			{
-				if (e.which === 37)
-				{
+	function onPlayerKeyDown(e) {
+		if ($(".html5-video-player").is(":focus")) {
+			if (e.ctrlKey) {
+				if (e.which === 37) { // Handle ctl + left key
 					var previousButton = $("a.ytp-prev-button, div.ytp-button-prev")[0];
 					if (previousButton)
 						previousButton.click();
 				}
-				else if (e.which === 39)
-				{
+				else if (e.which === 39) { // Handle ctl + right key
 					var nextButton = $("a.ytp-next-button, div.ytp-button-next")[0]
 					if (nextButton)
 						nextButton.click();
@@ -232,86 +207,84 @@ function installPlayerKeyboardShortcutExtensions()
 			}
 		}
 	}
-	
-	$(document).on("keydown", onPlayerKeyDown);	
+
+	$(document).on("keydown", onPlayerKeyDown);
+
+	$(".html5-video-player").on("keydown", function (e) {
+		if (e.which === 38 || e.which === 40) {
+			console.log("up or down");
+			e.stopPropagation();
+		}
+	});
 }
 
 // Expands video description
-function expandVideoDescription()
-{
-	$("#action-panel-details").removeClass("yt-uix-expander-collapsed");	
+function expandVideoDescription() {
+	$("#action-panel-details").removeClass("yt-uix-expander-collapsed");
 }
 
-// Correct the positioning of the playlist on window resize. 
-function installPlaylistRepositioner()
-{
+// Correct the positioning of the playlist on window resize.
+function installPlaylistRepositioner() {
 	$("#watch-appbar-playlist").removeClass("player-height");
-	
-	function onResize()
-	{
+
+	function onResize() {
 		var playlistContainer = $("#watch-appbar-playlist");
-		
+
 		if (playlistContainer.length === 0)
 			return;
-		
+
 		var playlistOffset = playlistContainer.offset();
 		playlistOffset.top = $("#watch-header").offset().top;
 		playlistContainer.offset(playlistOffset);
 	}
-	
+
 	onResize();
 	$(window).resize(onResize);
 }
 
 // Pauses playing videos in other tabs when a video play event is detected (works in both watch and channel page videos)
-function installPlayerAutoPause()
-{
+function installPlayerAutoPause() {
 	var videoPlayer = getVideoPlayer();
 
-	if (videoPlayer.length === 0)
-	{
+	if (videoPlayer.length === 0) {
 		//console.log("Player not found, retrying in 100ms..");
 		setTimeout(installPlayerAutoPause, 100);
 		return;
 	}
-	
+
 	var videoPlayerElement = videoPlayer.get(0);
-	
+
 	// Generate a random script instance ID
-	var instanceID = Math.random().toString();		
-	
-	function onVideoPlay()
-	{
+	var instanceID = Math.random().toString();
+
+	function onVideoPlay() {
 		localStorage["YouTubeCinemaMode_PlayingInstanceID"] = instanceID;
-		
-		function pauseWhenAnotherPlayerStartsPlaying()
-		{
+
+		function pauseWhenAnotherPlayerStartsPlaying() {
 			if (localStorage["YouTubeCinemaMode_PlayingInstanceID"] !== instanceID)
 				videoPlayerElement.pause();
 			else
 				setTimeout(pauseWhenAnotherPlayerStartsPlaying, 10);
 		}
-		
+
 		pauseWhenAnotherPlayerStartsPlaying();
 	}
-	
+
 	// If video isn't paused on startup, fire the handler immediately
 	if (!videoPlayerElement.paused)
 		onVideoPlay();
-	
+
 	// Add event handler for the "play" event.
 	videoPlayer.on("play", onVideoPlay);
 }
 
 // Get the video player element
-function getVideoPlayer()
-{
+function getVideoPlayer() {
 	// Note: the channel page has another hidden video except the main one (if it exists). The hidden video doesn't have an "src" attribute.
-	return $('video.html5-main-video').filter(function (index) { return $(this).attr("src") !== undefined});
+	return $('video.html5-main-video').filter(function (index) { return $(this).attr("src") !== undefined });
 }
 
 // Get the top bar element
-function getTopBar()
-{
+function getTopBar() {
 	return $("#masthead-positioner");
 }
